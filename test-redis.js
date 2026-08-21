39cbc4de46a5d9e0892d4dc77c84bc4529bc3800
sandeep392005-alt <sandeep392005@gmail.com>
2026-08-22 00:36:56 +0530

    test

--- test-redis.js
+++
@@ -1,6 +0,0 @@
-const { Redis } = require('ioredis');
-const redis = new Redis('redis://localhost:6379');
-redis.keys('*').then(keys => {
-  console.log('Redis Keys:', keys);
-  process.exit(0);
-}).catch(console.error);
